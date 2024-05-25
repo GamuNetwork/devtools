c6655a1c695b1bd7b2bca5845c0d13c3c1c32c51
Antoine Buirey <antoine.buirey@gmail.com>
2024-05-26 00:18:29 +0200

    adding module creation script

---
+++ create-module/customTypes.py
@@ -0,0 +1,82 @@
+from enum import Enum
+
+class Version:
+    def __init__(self, major : int, minor : int = 0, revision : int = 0):
+        self.major = major
+        self.minor = minor
+        self.revision = revision
+    
+    def __str__(self):
+        return f"{self.major}.{self.minor}.{self.revision}"
+    
+    def __eq__(self, other):
+        return self.major == other.major and self.minor == other.minor and self.revision == other.revision
+    
+    def __lt__(self, other):
+        if self.major < other.major:
+            return True
+        elif self.major == other.major:
+            if self.minor < other.minor:
+                return True
+            elif self.minor == other.minor:
+                return self.revision < other.revision
+        return False
+    
+    def __gt__(self, other):
+        if self.major > other.major:
+            return True
+        elif self.major == other.major:
+            if self.minor > other.minor:
+                return True
+            elif self.minor == other.minor:
+                return self.revision > other.revision
+        return False
+    
+    def __le__(self, other):
+        return self < other or self == other
+    
+    def __ge__(self, other):
+        return self > other or self == other
+    
+    def __ne__(self, other):
+        return not self == other
+    
+    def nextMajor(self):
+        return Version(self.major + 1)
+    
+    def nextMinor(self):
+        return Version(self.major, self.minor + 1)
+    
+    def nextRevision(self):
+        return Version(self.major, self.minor, self.revision + 1)
+    
+    @staticmethod
+    def fromString(version):
+        major, minor, revision = version.split(".")
+        return Version(int(major), int(minor), int(revision))
+    
+    @staticmethod
+    def isValid(version):
+        try:
+            Version.fromString(version)
+            return True
+        except:
+            return False    
+
+class ModuleTypes(Enum):
+    GAME = 1
+    INTERFACE = 2
+    OTHER = 3
+    
+    def __str__(self) -> str:
+        return self.name.lower()
+    
+    @staticmethod
+    def fromString(type : str) -> 'ModuleTypes':
+        type = type.lower()
+        if type == "game":
+            return ModuleTypes.GAME
+        elif type == "interface":
+            return ModuleTypes.INTERFACE
+        else:
+            return ModuleTypes.OTHER
---
+++ create-module/main.py
@@ -0,0 +1,86 @@
+
+from zipfile import ZipFile
+import os
+import json
+from datetime import datetime
+from customTypes import Version, ModuleTypes
+
+from gamuLogger import debug, info, error, critical, Printer
+
+def createJson(module_name, module_version : Version, module_type : ModuleTypes, module_description, module_author, branch = "main"):
+    debug(f"Creating json file for module {module_name} version {module_version}")
+    module = {
+        "name": module_name,
+        "version": str(module_version),
+        "type": str(module_type),
+        "description": module_description,
+        "author": module_author,
+        "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "repository": f"https://github.com/GamuNetwork/{module_name}",
+        "branch": branch
+    }
+    result = json.dumps(module, indent=4)
+    debug(f"Json file created: {result}")
+    return result
+
+def addFolderToZip(zip: ZipFile, arc_path, folder, parent_folder = ""):
+    debug(f"Adding folder {folder} to zip")
+    for file in os.listdir(folder):
+        file_path = os.path.join(folder, file)
+        if os.path.isfile(file_path):
+            debug(f"Adding file {file} to zip")
+            zip.write(file_path, os.path.join(arc_path, parent_folder, file))
+        elif os.path.isdir(file_path):
+            addFolderToZip(zip, arc_path, file_path, os.path.join(parent_folder, file))
+
+def createArchive(compiled_code_folder, module_name, module_version : Version, module_type : ModuleTypes, module_description, module_author, branch = "main"):
+    with ZipFile(f'{module_name}-{str(module_version)}.gamod', 'w') as zip:
+        addFolderToZip(zip, "build", compiled_code_folder)
+        debug(f"Compiled code folder added to archive")
+        zip.writestr('module.json', createJson(module_name, module_version, module_type, module_description, module_author, branch))
+        debug(f"Json file added to archive")
+    
+    return f'{module_name}-{str(module_version)}.gamod'
+
+if __name__ == "__main__":
+    import argparse
+    
+    def handleParserError(message):
+        error(message)
+        raise ValueError("Invalid arguments")
+    
+    def createParser():
+        parser = argparse.ArgumentParser(description="Create a module archive")
+        parser.error = handleParserError
+        parser.add_argument("compiled_code_folder", help="The compiled code folder (dist folder for javascript)")
+        parser.add_argument("module_name", help="The module name")
+        parser.add_argument("module_version", help="The module version")
+        parser.add_argument("module_type", help="The module type")
+        parser.add_argument("module_description", help="The module description")
+        parser.add_argument("module_author", help="The module author")
+        parser.add_argument("--branch", help="The branch of the repository", default="main")
+        parser.add_argument("--debug", "-d", help="Enable debug mode", action="store_true")
+        return parser
+    
+    info("Starting module creation")
+    
+    try:
+        parser = createParser()
+        args = parser.parse_args()
+        
+        if args.debug:
+            Printer().set_level(Printer.LEVELS.DEBUG)
+    
+        module_version = Version.fromString(args.module_version)
+        module_type = ModuleTypes.fromString(args.module_type)
+
+        info(f"Creating archive for module {args.module_name} version {module_version}")
+        
+        createArchive(args.compiled_code_folder, args.module_name, module_version, module_type, args.module_description, args.module_author, args.branch)
+    
+    except Exception as e:
+        critical(f"Error creating module: {str(e)}")
+        exit(1)
+    
+    info(f"Archive created: {args.module_name}-{str(module_version)}.gamod")
+    exit(0)
