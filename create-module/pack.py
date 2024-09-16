a6bdf640df7f24c547597851f50bef1c653c34be
Antoine <91411073+T0ine34@users.noreply.github.com>
2024-09-16 16:35:20 +0200

    updating create-module; now built using feanor

--- create-module/pack.py
+++ create-module/pack.py
@@ -1,11 +1,11 @@
-from builderTool import BaseBuilder, PYTHON #this file use the module to build itself
+from feanor import BaseBuilder
 
 
 class Builder(BaseBuilder):
     def Setup(self):
         self.addDirectory('src', 'src/createModule')
         self.addAndReplaceByPackageVersion('pyproject.toml')
-        self.runCommand(f'{PYTHON} -m pip install --upgrade build')
+        self.venv().install('build')
         
     def Build(self):
-        self.runCommand(f'{PYTHON} -m build --outdir {self.distDir} .')
+        self.venv().runModule(f"build --outdir {self.distDir} .")
